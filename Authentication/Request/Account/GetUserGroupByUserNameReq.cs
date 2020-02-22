0530dbb53f804b53a5d194449a11854a24e32ab2
mrhaiuit <davidtrinh173@gmail.com>
2020-02-22 16:03:09 +0700

    commit;

---
+++ Authentication/Request/Account/GetUserGroupByUserNameReq.cs
@@ -0,0 +1,23 @@
+﻿using EVE.Commons;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Authentication.Request
+{
+    [Validator(typeof(GetUserGroupByUserNameReqValidator))]
+    public class GetUserGroupByUserNameReq
+    {
+        public string UserName { get; set; }
+    }
+
+    public class GetUserGroupByUserNameReqValidator : AbstractValidator<GetUserGroupByUserNameReq>
+    {
+        public GetUserGroupByUserNameReqValidator()
+        {
+            RuleFor(c => c.UserName)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage(((int) EnumError.UsernameIsNullOrEmpty).ToString());
+        }
+    }
+}
