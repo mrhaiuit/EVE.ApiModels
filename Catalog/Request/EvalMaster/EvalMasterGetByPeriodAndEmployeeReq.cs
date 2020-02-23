2dc327f80ba887b8a5f79002a473ac1448127f04
mrhaiuit <davidtrinh173@gmail.com>
2020-02-23 15:39:10 +0700

    commit;

---
+++ Catalog/Request/EvalMaster/EvalMasterGetByPeriodAndEmployeeReq.cs
@@ -0,0 +1,36 @@
+﻿using EVE.Commons;
+using EVE.WebApi.Shared;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Catalog
+{
+    [Validator(typeof(EvalMasterGetByPeriodAndEmployeeValidator))]
+    public class EvalMasterGetByPeriodAndEmployeeReq 
+    {
+        public int PeriodId { get; set; }
+        public int BeEvalEmployeeId { get; set; }
+        public int EvalEmployeeId { get; set; }
+    }
+
+    public class EvalMasterGetByPeriodAndEmployeeValidator : AbstractValidator<EvalMasterGetByPeriodAndEmployeeReq>
+    {
+        public EvalMasterGetByPeriodAndEmployeeValidator()
+        {
+            RuleFor(c => c.PeriodId)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage((EnumError.PeriodIdIsNullOrEmpty).ToString());
+
+            RuleFor(c => c.BeEvalEmployeeId)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage((EnumError.BeEvalEmployeeIdIsNullOrEmpty).ToString());
+
+            RuleFor(c => c.EvalEmployeeId)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage((EnumError.EvalEmployeeIdIsNullOrEmpty).ToString());
+        }
+    }
+}
---
+++ Catalog/Request/EvalMaster/ExeEvalDetailByMasterIdReq.cs
@@ -0,0 +1,36 @@
+﻿using EVE.Commons;
+using EVE.WebApi.Shared;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Catalog
+{
+    [Validator(typeof(ExeEvalDetailByMasterIdValidator))]
+    public class ExeEvalDetailByMasterIdReq 
+    {
+        public int BeEvalEmployeeId { get; set; }
+        public int EvalEmployeeId { get; set; }
+        public int PeriodId { get; set; }
+    }
+
+    public class ExeEvalDetailByMasterIdValidator : AbstractValidator<ExeEvalDetailByMasterIdReq>
+    {
+        public ExeEvalDetailByMasterIdValidator()
+        {
+            RuleFor(c => c.BeEvalEmployeeId)
+                    .NotNull()
+                    .NotEmpty()
+                     .WithMessage((EnumError.BeEvalEmployeeIdIsNullOrEmpty).ToString());
+
+            RuleFor(c => c.EvalEmployeeId)
+                    .NotNull()
+                    .NotEmpty()
+                     .WithMessage((EnumError.EvalEmployeeIdIsNullOrEmpty).ToString());
+
+            RuleFor(c => c.PeriodId)
+                    .NotNull()
+                    .NotEmpty()
+                     .WithMessage((EnumError.PeriodIdIsNullOrEmpty).ToString());
+        }
+    }
+}
