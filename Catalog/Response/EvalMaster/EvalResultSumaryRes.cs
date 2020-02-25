8cd1e97be5f22519ef5e20f772369cf14681756f
davidtrinh173 <davidtrinh173@gmail.com>
2020-02-25 17:39:40 +0700

    commit;

--- Catalog/Response/EvalMaster/EvalResultSumaryRes.cs
+++ Catalog/Response/EvalMaster/EvalResultSumaryRes.cs
@@ -5,11 +5,11 @@ namespace EVE.ApiModels.Catalog.Response
 {
     public class EvalResultSumaryRes 
     {
-        public int Total { get; set; }
-        public int Everage { get; set; } = 0;
-        public int TotalCriteria { get; set; } = 0;
-        public int TotalCriteriaHasValue { get; set; }
-        public string EvalResultCode { get; set; } = string.Empty;
-        public string EvalResultName { get; set; } = string.Empty;
+        public int Total { get; set; }  // Tổng điểm
+        public int Everage { get; set; } = 0; // Điểm trung bình
+        public int TotalCriteria { get; set; } = 0; // Tổng số tiêu chí
+        public int TotalCriteriaHasValue { get; set; } // Tổng số tiêu chí đã đánh giá
+        public string EvalResultCode { get; set; } = string.Empty; // Mã kết quả
+        public string EvalResultName { get; set; } = string.Empty; // Giá trị kết quả(Đat, không đạt ..)
     }
 }
