2b1130222a2ee5168b593eee7ae51faa95739bdb
davidtrinh173 <davidtrinh173@gmail.com>
2020-02-25 17:32:39 +0700

    commit;

---
+++ Catalog/Response/EvalMaster/EvalResultSumaryRes.cs
@@ -0,0 +1,15 @@
+﻿using EVE.Data;
+using System.Collections.Generic;
+
+namespace EVE.ApiModels.Catalog.Response
+{
+    public class EvalResultSumaryRes 
+    {
+        public int Total { get; set; }
+        public int Everage { get; set; } = 0;
+        public int TotalCriteria { get; set; } = 0;
+        public int TotalCriteriaHasValue { get; set; }
+        public string EvalResultCode { get; set; } = string.Empty;
+        public string EvalResultName { get; set; } = string.Empty;
+    }
+}
