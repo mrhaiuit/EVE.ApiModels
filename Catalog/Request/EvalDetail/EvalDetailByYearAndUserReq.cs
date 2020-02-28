15bbab5abeff443d99bff1fc537821daaaa60e38
mrhaiuit <davidtrinh173@gmail.com>
2020-02-28 21:23:05 +0700

    commit;

---
+++ Catalog/Request/EvalDetail/EvalDetailByYearAndUserReq.cs
@@ -0,0 +1,35 @@
+﻿using EVE.Commons;
+using EVE.WebApi.Shared;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Catalog
+{
+    [Validator(typeof(EvalDetailByYearAndUserValidator))]
+    public class EvalDetailByYearAndUserReq 
+    {
+        public int EvalCriteriaId { get; set; }
+        public int EmployeeId { get; set; }
+        public int Year { get; set; }
+
+    }
+
+    public class EvalDetailByYearAndUserValidator : AbstractValidator<EvalDetailByYearAndUserReq>
+    {
+        public EvalDetailByYearAndUserValidator()
+        {
+            RuleFor(c => c.EvalCriteriaId)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage(((int)EnumError.EvalCriteriaIdNullOrEmpty).ToString());
+            RuleFor(c => c.EmployeeId)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage(((int)EnumError.EmployeeIdIsNullOrEmpty).ToString());
+            RuleFor(c => c.Year)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage(((int)EnumError.YearIsNullOrEmpty).ToString());
+        }
+    }
+}
---
+++ Catalog/Response/EvalDetail/EvalDetailByYearAndUserRes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EVE.ApiModels.Catalog.Response
+{
+    public class EvalDetailByYearAndUserRes
+    {
+        public int Year { get; set; }
+        public string EvalResultName { get; set; }
+        public string Reason { get; set; }
+        public string Attachment { get; set; }
+        
+    }
+}
