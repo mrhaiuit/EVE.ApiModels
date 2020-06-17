3ced0fd4bfffe37d24eac13072d8acf878ec8774
mrhaiuit <mrhai.uit173@gmail.com>
2020-06-17 21:52:31 +0700

    Bổ sung evalmaster

---
+++ Catalog/Request/EvalMaster/EvalMasterGetByUserIdReq.cs
@@ -0,0 +1,30 @@
+﻿using EVE.Commons;
+using EVE.WebApi.Shared;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Catalog
+{
+    [Validator(typeof(EvalMasterGetByUserIdValidator))]
+    public class EvalMasterGetByUserIdReq 
+    {
+        public int EmployeeId { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class EvalMasterGetByUserIdValidator : AbstractValidator<EvalMasterGetByUserIdReq>
+    {
+        public EvalMasterGetByUserIdValidator()
+        {
+            RuleFor(c => c.EmployeeId)
+                    .NotNull()
+                    .NotEmpty()
+                     .WithMessage((EnumError.EmployeeIdIsNullOrEmpty).ToString());
+
+            RuleFor(c => c.Year)
+                    .NotNull()
+                    .NotEmpty()
+                     .WithMessage((EnumError.YearCanNotBeNull).ToString());
+        }
+    }
+}
---
+++ Catalog/Response/EvalMaster/EvalMasterGetByUserIdRes.cs
@@ -0,0 +1,20 @@
+﻿using EVE.Data;
+using System;
+using System.Collections.Generic;
+
+namespace EVE.ApiModels.Catalog.Response
+{
+    public class EvalMasterGetByUserIdRes
+    {
+        public int MasterId { get; set; }  // Tổng điểm
+        public int? Year { get; set; }
+        public string Period { get; set; }
+        public string EvalEmployee { get; set; }
+        public string BeEvalEmployee { get; set; } 
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public bool? Approveed { get; set; }
+        public int TotalEval { get; set; } = 0;
+        public string Class { get; set; }
+    }
+}
