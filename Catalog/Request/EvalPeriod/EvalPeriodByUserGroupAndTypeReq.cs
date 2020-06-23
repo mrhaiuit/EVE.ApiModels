67a108f56caa2da139795529bb5b19e01f2a8abf
mrhaiuit <mrhai.uit173@gmail.com>
2020-06-23 14:25:27 +0700

    Commit;

---
+++ Catalog/Request/EvalPeriod/EvalPeriodByUserGroupAndTypeReq.cs
@@ -0,0 +1,34 @@
+﻿using EVE.Commons;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Catalog
+{
+
+    [Validator(typeof(EvalPeriodByUserGroupAndTypeValidator))]
+    public class EvalPeriodByUserGroupAndTypeReq 
+    {
+        public string UserGroupCode { get; set; }
+        public int EmpoyeeId { get; set; }
+        public string EvalTypeCode { get; set; }
+    }
+
+    public class EvalPeriodByUserGroupAndTypeValidator : AbstractValidator<EvalPeriodByUserGroupAndTypeReq>
+    {
+        public EvalPeriodByUserGroupAndTypeValidator()
+        {
+            RuleFor(c => c.UserGroupCode)
+                    .NotNull()
+                    .NotEmpty()
+                  .WithMessage((EnumError.UserGroupCodeIsNullOrEmpty).ToString());
+            RuleFor(c => c.EmpoyeeId)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage((EnumError.EmployeeIdIsNullOrEmpty).ToString());
+            RuleFor(c => c.EvalTypeCode)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage((EnumError.EvalTypeCodeIsNullOrEmpty).ToString());
+        }
+    }
+}
