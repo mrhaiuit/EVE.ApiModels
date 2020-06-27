414e144e97717ec6425ae50e0bc54316881e6c14
mrhaiuit <mrhai.uit173@gmail.com>
2020-06-27 09:01:55 +0700

    commit;

---
+++ Catalog/Response/EvalCriteria/EvalCriteriaSchoolLevelRes.cs
@@ -0,0 +1,17 @@
+﻿using EVE.Data;
+using System;
+using System.Collections.Generic;
+
+namespace EVE.ApiModels.Catalog.Response
+{
+    public class EvalCriteriaSchoolLevelRes 
+    {
+        public int EvalCriteriaId { get; set; }
+        public Nullable<int> EvalStandardId { get; set; }
+        public string EvalCriteriaCode { get; set; }
+        public string EvalCriteriaName { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public string Idx { get; set; }
+        public string EvalStandardName { get; set; } 
+    }
+}
