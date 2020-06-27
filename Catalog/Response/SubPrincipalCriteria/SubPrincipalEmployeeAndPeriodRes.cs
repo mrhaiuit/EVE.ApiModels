f69add51970cb1b65c4e69d3541b21f4b97cc635
mrhaiuit <mrhai.uit173@gmail.com>
2020-06-27 09:53:56 +0700

    commit

---
+++ Catalog/Response/SubPrincipalCriteria/SubPrincipalEmployeeAndPeriodRes.cs
@@ -0,0 +1,16 @@
+﻿using EVE.Data;
+using System;
+using System.Collections.Generic;
+
+namespace EVE.ApiModels.Catalog.Response
+{
+    public class SubPrincipalEmployeeAndPeriodRes
+    {
+        public int SubPrincipalId { get; set; }
+        public int EvalPeriodId { get; set; }
+        public int EvalCriteriaId { get; set; }
+        public string EvalCriteriaName { get; set; }
+        public int Idx { get; set; }
+        public string EvalStandardName { get; set; } 
+    }
+}
